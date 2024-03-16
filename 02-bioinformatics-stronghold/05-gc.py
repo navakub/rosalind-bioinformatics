50580a53e8131be8ec997c3998449777ae2f016e
navakub <dulyawat.boonvut@gmail.com>
2024-03-16 18:23:43 +0700

    practice

--- 02-bioinformatics-stronghold/05-gc.py
+++ 02-bioinformatics-stronghold/05-gc.py
@@ -1,7 +1,22 @@
 from os import path
+from collections import Counter
 
-tol = 0.001
+script_dir = path.dirname(__file__) #<-- absolute dir the script is in
+rel_path = '05-gc.fas'
+abs_file_path = path.join(script_dir, rel_path)
 
-file_path = path.relpath('05-gc.fas')
-with open(file_path) as f:
-    print(1)
+label_list = []
+gc_list = []
+with open(abs_file_path) as f:
+    for dna in f.readlines():
+        if dna[0] == '>':
+            label = dna[1:-1]
+            label_list.append(label)
+        else:
+            gc = 100 * (dna.count('G') + dna.count('C'))/ len(dna)
+            gc_list.append(gc)
+
+idx = gc_list.index(max(gc_list))
+
+print(label_list[idx])
+print(f'{gc_list[idx]:.6f}')
