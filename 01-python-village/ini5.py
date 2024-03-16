255eff9a1051d925f61d9780285737b5de13753e
navakub <dulyawat.boonvut@gmail.com>
2024-03-16 18:32:38 +0700

    practice

--- 01-python-village/ini5.py
+++ 01-python-village/ini5.py
@@ -1,10 +1,12 @@
-import os.path
+from os import path
 
-filename = os.path.abspath('./python-village/ini5.txt')
-f = open(filename, 'r')
+script_dir = path.dirname(__file__) #<-- absolute dir the script is in
+rel_path = 'ini5.txt'
+abs_file_path = path.join(script_dir, rel_path)
 
-i = 1
-for line in f.readlines():
-    if i % 2 == 0 :
-        print(line)
-    i += 1
+with open(abs_file_path) as f:
+    i = 1
+    for line in f.readlines():
+        if i % 2 == 0 :
+            print(line)
+        i += 1
